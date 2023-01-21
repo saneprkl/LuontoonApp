18a4aba32e430194446e1cb92d733feceb12d797
saneprkl <santeri.lehtonen@gmail.com>
2023-01-21 02:41:28 +0200

    Location denied

--- app/src/main/java/com/example/luontoonapp/PermissionHelper.kt
+++ app/src/main/java/com/example/luontoonapp/PermissionHelper.kt
@@ -7,7 +7,13 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import android.Manifest
 import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 
 class PermissionHelper(private val activity: Activity) {
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
@@ -20,8 +26,19 @@ class PermissionHelper(private val activity: Activity) {
                 Toast.makeText(
                     activity,
                     "Location is required to function properly",
-                    Toast.LENGTH_LONG
-                ).show()
+                    Toast.LENGTH_LONG).show()
+
+                val settingsButton = Button(activity)
+                settingsButton.text = "Open settings"
+                settingsButton.setOnClickListener {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.fromParts("package", activity.packageName, null))
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    activity.startActivity(intent)
+                }
+                activity.addContentView(settingsButton,
+                    ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT))
             } else {
                 ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 LOCATION_PERMISSION_REQUEST_CODE)
