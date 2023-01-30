28c7b83656516973ad05f71fe4dbdb38a4d99239
saneprkl <santeri.lehtonen@gmail.com>
2023-01-30 20:28:18 +0200

    Frontpage layouting!

--- app/src/main/java/com/example/luontoonapp/SecondFragment.kt
+++ app/src/main/java/com/example/luontoonapp/SecondFragment.kt
@@ -21,6 +21,7 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationRequest
+import android.os.Looper
 import android.widget.Toast
 import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.tasks.CancellationToken
@@ -63,6 +64,8 @@ class SecondFragment : Fragment() {
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
             fusedLocationClient.lastLocation.addOnFailureListener { println("Location not found") }
             //fusedLocationClient.lastLocation.addOnSuccessListener
+
+
             fusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
                 override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
                 override fun isCancellationRequested() = false
@@ -70,15 +73,21 @@ class SecondFragment : Fragment() {
                 if (location == null) {
                     Toast.makeText(requireContext(), "Cannot get location.", Toast.LENGTH_SHORT).show()
                 } else {
+
                     val mapView = view.findViewById<MapView>(R.id.mapView)
                     mapView.setTileSource(TileSourceFactory.MAPNIK)
                     mapView.setMultiTouchControls(true)
 
                     var longitude = location.longitude
                     var latitude = location.latitude
-                    val userLocation = Marker(mapView)
-                    val geoPoint = GeoPoint(latitude, longitude)
+                    var userLocation = Marker(mapView)
+                    var geoPoint = GeoPoint(latitude, longitude)
                     userLocation.position = geoPoint
+
+                    var mapController = mapView.controller
+                    var start = GeoPoint(latitude, longitude)
+                    mapController.setCenter(start)
+                    mapController.setZoom(14.5)
 
                     userLocation.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
                     userLocation.title = "You're here!"
@@ -86,17 +95,12 @@ class SecondFragment : Fragment() {
                         requireContext(),
                         R.drawable.baseline_person_pin_circle_24
                     )
-                    val mapController = mapView.controller
-                    val start = GeoPoint(latitude, longitude)
-                    mapController.setCenter(start)
-                    mapController.setZoom(14.5)
 
                     mapView.overlays.add(userLocation)
                 }
             }
 
         }
-
     }
 
     override fun onDestroyView() {
